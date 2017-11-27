da4cbc773c742a8102f1b4bea41703350b8dc991
IcusFMagni <danika.ragnhild@gmail.com>
2017-11-27 10:29:21 -0600

    added put and delete

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
             res.sendStatus(500);
         } else {
 
-            client.query('SELECT * FROM tasks;', function (errorMakingQuery, result) {
+            client.query('SELECT * FROM tasks ORDER BY is_complete, id;', function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
                     console.log('Error making query', errorMakingQuery);
@@ -43,6 +43,42 @@ router.post('/', function (req,res) {
     })
 })
 
+router.delete('/complete/:id', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query(`DELETE FROM tasks WHERE id=$1;`, [req.params.id], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+});
+
+router.put('/delete/:id', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done){
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase)
+            res.sendStatus(500);
+        } else {
+            client.query('UPDATE tasks SET is_complete=true WHERE id=$1', [req.params.id], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    res.sendStatus(500)
+                } else {
+                    res.sendStatus(200)
+                }
+            })
+        }
+    })
+})
 
 
 module.exports= router;
